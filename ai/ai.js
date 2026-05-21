5617ec18b83e5f41c756006883bd1bcaac784889
Saiida <saiida.mitalipova@alatoo.edu.kg>
2026-05-21 16:08:10 +0600

    Move API key to .env for security

--- ai/ai.js
+++ ai/ai.js
@@ -1,18 +1,17 @@
+require('dotenv').config()
 const Groq = require("groq-sdk");
 
 const groq = new Groq({
-  apiKey: ""
+  apiKey: process.env.GROQ_API_KEY  // убрали хардкод
 });
 
 async function getGroqChatCompletion({user_message}) {
   return groq.chat.completions.create({
     messages: [
-
       {
         role: "system",
         content: `You are SAT tutor help to student prep for the exam`
-    },
-
+      },
       {
         role: "user",
         content: user_message
